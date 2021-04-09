04f04392a121b87a83f56af9d996d346750d60ca
rintukutum <rintukutum@gmail.com>
2021-04-09 12:42:22 +0530

    updated code

--- 00-organize-person-level-data.R
+++ 00-organize-person-level-data.R
@@ -24,7 +24,19 @@ q1_measure <- readr::read_csv(
 length(unique(q1_measure$measurement_concept_id))
 #[1] 492
 save(q1_measure,file='../data/q1trMeasure.RData')
-# In case connectio breaks
+# colnames(q1_measure)
+# [1] "measurement_id"                "person_id"                    
+# [3] "measurement_concept_id"        "measurement_date"             
+# [5] "measurement_datetime"          "measurement_time"             
+# [7] "measurement_type_concept_id"   "operator_concept_id"          
+# [9] "value_as_number"               "value_as_concept_id"          
+# [11] "unit_concept_id"               "range_low"                    
+# [13] "range_high"                    "provider_id"                  
+# [15] "visit_occurrence_id"           "visit_detail_id"              
+# [17] "measurement_source_value"      "measurement_source_concept_id"
+# [19] "unit_source_value"             "value_source_value"
+
+# In case connection breaks
 # 
 # load('../data/q1trMeasure.RData')
 
@@ -39,5 +51,68 @@ q1y0.measure <- q1_measure[q1_measure$person_id %in% q1y0$person_id,]
 # convert to specific format
 # measurement names and codes
 # temporal person specific data
+
+person.data.y1 <- plyr::dlply(q1y1.measure,'person_id')
+person.data.y0 <- plyr::dlply(q1y0.measure,'person_id')
+
+mzPerson <- rbind(
+    data.frame(meauresPerPerson = sapply(person.data.y1,nrow),
+          y='positive',stringsAsFactors = FALSE),
+    data.frame(meauresPerPerson = sapply(person.data.y0,nrow),
+          y='negative',stringsAsFactors = FALSE)
+  )
+
+
+library(ggplot2)
+library(ggpubr)
+p <- ggplot(mzPerson,aes(x=meauresPerPerson)) +
+  geom_histogram() +
+  facet_wrap(facets = "y") +
+  theme_pubr()
+dir.create('./figures',showWarnings = FALSE)
+
+pdf('./figures/00-measurements-per-person.pdf',
+    width = 5,height = 5)
+p
+dev.off()
+
+# cat ./data/q1-081920/training/location.csv | awk '{split($0,a,","); print a[6]}' | uniq 
+library(reshape2)
+xtest <- person.data.y1[[2]]
+
+xwide <- dcast(
+  xtest,
+  person_id~measurement_concept_id,
+  value.var = "value_as_number",fun.aggregate = mean
+)
+
+pDF.y1 <- plyr::ldply(lapply(person.data.y1,function(x){
+  dcast(
+    x,
+    person_id~measurement_concept_id,
+    value.var = "value_as_number",fun.aggregate = mean
+  )
+}))
+
+pDF.y0 <- plyr::ldply(lapply(person.data.y0,function(x){
+  dcast(
+    x,
+    person_id~measurement_concept_id,
+    value.var = "value_as_number",fun.aggregate = mean
+  )
+}))
+
+save(pDF.y0,pDF.y1, file = '../data/pDF.measure.RData')
+
+
+
+
+
+
+
+
+
+
+
 
 
